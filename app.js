036a3f0d58885dcfd148b3c33b3662d634b153f0
fisikasi <ibonrec@gmail.com>
2026-06-01 12:45:31 +0200

    deskargatzeko botoia

--- app.js
+++ app.js
@@ -1057,3 +1057,30 @@ function shuffle(array) {
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("service-worker.js");
 }
+
+let deferredPrompt = null;
+
+const installBtn = document.getElementById("installBtn");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  deferredPrompt = event;
+
+  installBtn.classList.remove("hidden");
+});
+
+installBtn.addEventListener("click", async () => {
+  if (!deferredPrompt) return;
+
+  deferredPrompt.prompt();
+
+  await deferredPrompt.userChoice;
+
+  deferredPrompt = null;
+  installBtn.classList.add("hidden");
+});
+
+window.addEventListener("appinstalled", () => {
+  deferredPrompt = null;
+  installBtn.classList.add("hidden");
+});
